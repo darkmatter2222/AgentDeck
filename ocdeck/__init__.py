a91af8e2f1e94f75ceedca8a15fe4a5313cf6860
Ryan V. Susman <ryansusman@gmail.com>
2026-09-13 16:19:55 -0400

    Enable Jelly PyPI update integration

--- ocdeck/__init__.py
+++ ocdeck/__init__.py
@@ -5,4 +5,9 @@ from .jelly_cute import install as _install_jelly_cute
 
 _install_jelly_cute(_jelly_art)
 
-del _install_jelly_cute, _jelly_art
+from . import device as _device
+from .jelly_update import install_device_patch as _install_jelly_update
+
+_install_jelly_update(_device.DeviceLoop)
+
+del _device, _install_jelly_cute, _install_jelly_update, _jelly_art
